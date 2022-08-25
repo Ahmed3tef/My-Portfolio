663f462ebf8a30ac26f65c3479e0c16bf34e409c
Ahmed Atef <atteff101@gmail.com>
2022-08-26 00:33:37 +0200

    finished dev

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,58 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import {
+  AiFillFacebook,
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:+201093505489'>+20-1093505489</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href='mailto:atteff101@gmail.com'>Mail me here!</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href='https://github.com/Ahmed3tef' target='_blank'>
+            <AiFillGithub style={{ fontSize: '3rem' }}></AiFillGithub>
+          </SocialIcons>
+          <SocialIcons
+            href='https://www.linkedin.com/in/ahmed-atef-al-atafy-417a951a9/'
+            target='_blank'>
+            <AiFillLinkedin style={{ fontSize: '3rem' }}></AiFillLinkedin>
+          </SocialIcons>
+          <SocialIcons
+            href='https://www.facebook.com/A7mEeDO0O'
+            target='_blank'>
+            <AiFillFacebook style={{ fontSize: '3rem' }}></AiFillFacebook>
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,68 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import { SiAdobephotoshop, SiReact, SiJavascript } from 'react-icons/si';
+import { GoDatabase } from 'react-icons/go';
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id='tech'>
+    <SectionDivider divider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range a technologies in the web development world.
+      Front-end to Back-end.
+    </SectionText>
+    <List>
+      <ListItem>
+        <picture>
+          <SiReact size='3rem' />
+        </picture>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <GoDatabase size='3rem' />
+        </picture>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <SiAdobephotoshop size='3rem' />
+        </picture>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Figma & PS
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <SectionDivider colorAlt />
+  </Section>
 );
 
 export default Technologies;
